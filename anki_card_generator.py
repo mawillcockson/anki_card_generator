aea50002d400d20e0d6e7aea51b34b23d21c0219
Matthew Willcockson <matthew@willcockson.family>
2020-04-19 16:21:34 -0500

    (feature)Vocab and character matching works
    Have only checked the syntax in the file, and run mypy against it.

--- anki_card_generator.py
+++ anki_card_generator.py
@@ -8,32 +8,45 @@ import sys
 from numbers import Number
 from pathlib import Path
 from re import compile as re_compile
-from typing import (
-    Any,
-    Callable,
-    List,
-    NamedTuple,
-    NewType,
-    Optional,
-    Tuple,
-    TypedDict,
-    TypeVar,
-    Union,
-    Dict,
-)
+from itertools import compress
+from operator import not_
 
 from argparse import ArgumentParser, ArgumentTypeError, Namespace
 import logging
 from invoke import run
 
+try:
+    from typing import (
+        Any,
+        Callable,
+        List,
+        NamedTuple,
+        NewType,
+        Optional,
+        Tuple,
+        TypedDict,
+        TypeVar,
+        Union,
+        Dict,
+        Iterator,
+    )
+except ImportError as err:
+    assert sys.version_info >= (3, 8), "Requires Python 3.8 or higher"
+    sys.exit("Could not import needed types from typing module")
+
 ## Program defaults
 PROG_NAME = sys.argv[0]
 default_media_dir = "~/scoop/persist/anki/data/User 1/collection.media"
 default_log_level = logging.WARN
+rtk_index_file = "./rtk_index.txt"
 
 ## Classes
 comment_re = re_compile(r"^\s*(#|＃).*$")
 blank_re = re_compile(r"^\s+$")
+ends_with_keyword_re = re_compile(r"^[\w\s]+[ a-zA-Z-]+$")
+character_and_keyword_re = re_compile(r"^(?P<character>\w)\s+(?P<keyword>\w+)$")
+character_index_re = re_compile(r"^(?P<index>\d+|\*)\s+(?P<character>\w)$")
+story_re = re_compile(r"^story:\s+(?P<story>\w.*)$")
 Size = NamedTuple("Size", [("height", Number), ("width", Number)])
 Position = NamedTuple("Position", [("x", Number), ("y", Number)])
 SPath = Union[Path, str]
@@ -44,7 +57,7 @@ Num = Union[int, float]
 class CharacterGroup(NamedTuple):
     character: str
     keyword: str
-    frame_number: Optional[int]
+    frame_number: Optional[Union[int, str]]
     story: str
 
 
@@ -72,6 +85,26 @@ class Notes(Files):
 
 
 ## Functions
+def error(message: str) -> Exception:
+    logging.error(message.lstrip())
+    return Exception(message.lstrip())
+
+
+def split_groups(paragraph: str) -> List[str]:
+    assert isinstance(paragraph, str), f"'{paragraph}' is not a str"
+    lines = paragraph.splitlines()
+    current_paragraph: List[str] = list()
+    paragraphs: List[str] = list()
+    for line in lines:
+        if line == "":
+            paragraphs.append("\n".join(current_paragraph))
+        else:
+            current_paragraph.append(line)
+    if len(current_paragraph) > 0:
+        paragraphs.append("\n".join(current_paragraph))
+    return paragraphs
+
+
 def check_collisions(lines: List[str], directory: Union[Path, str]) -> Dict[str, Path]:
     dir_path = Path(directory)
     if not (dir_path.is_dir() and dir_path.exists()):
@@ -90,23 +123,117 @@ def check_collisions(lines: List[str], directory: Union[Path, str]) -> Dict[str,
         )
     return {file.stem: file for file in dir_contents}
 
-def comment_or_blank(string: str) -> bool:
-    return bool(comment_re.search(string) or blank_re.search(string) or string == "")
+
+def not_comment_or_blank(string: str) -> bool:
+    return not (comment_re.search(string) or blank_re.search(string) or string == "")
+
+
+# I was thinking these could be arpeggio-based parsers, just for fun
+def parse_vocab_group(paragraph: str) -> VocabGroup:
+    assert isinstance(paragraph, str), f"'{paragraph}' is not str"
+    lines = paragraph.splitlines()
+    story_lines = list(map(story_re.search, lines))
+    story_matches = list(filter(None, story_lines))
+    non_stories = list(compress(lines, map(not_, story_lines)))
+    if len(non_stories) < 3:
+        raise error(
+            f"""Vocab group has word on line 1, definition on line 2, and 1 or more
+                example sentences on lines following, with an optional story/mnemonic
+                indicated by a line starting with 'story: '
+                Does not match:
+                {paragraph}"""
+        )
+    if len(story_matches) > 0:
+        story = story_matches[0].group(0)
+    else:
+        story = ""
+    return VocabGroup(
+        word=non_stories[0],
+        definition=non_stories[1],
+        example_sentences=non_stories[2:],
+        story=story,
+    )
+
+
+def rtk_index_gen(rtk_index_file: SPath) -> Callable[[str], Union[int, str, None]]:
+    """Expects a file to be present in the same directory, where each line
+    contains the frame number and corresponding character from Remembering the Kanji vol1, v6"""
+    rtk_index = Path(rtk_index_file)
+    if not rtk_index.is_file():
+        raise error(f"Expected to find file '{rtk_index}'")
+    lines = rtk_index.read_text().splitlines()
+    match_list = list(map(character_index_re.search, lines))
+    non_matches = list(compress(lines, map(lambda x: x == None, match_list)))
+    for i, non_match in enumerate(non_matches):
+        logging.error(
+            f"line {i} does not match pattern of number or '*', a space, and a character: {non_match}"
+        )
+    if len(non_matches) > 0:
+        raise ValueError(f"Does not match format:\n{non_matches}")
+
+    matches = filter(None, match_list)
+    lookup_dict: Dict[str, Union[int, str]] = dict()
+    for match in matches:
+        index, character = match.groups()
+        if index.isdigit():  # Recognizes full-width characters
+            lookup_dict[character] = int(index)
+        else:
+            lookup_dict[character] = "*"
+
+    def lookup(character: str) -> Union[int, str, None]:
+        return lookup_dict.get(character)
+
+    return lookup
+
+
+character_to_frame_number = rtk_index_gen(rtk_index_file)
+
+
+def parse_character_group(paragraph: str) -> CharacterGroup:
+    assert isinstance(paragraph, str), f"'{paragraph}' is not a str"
+    lines = paragraph.splitlines()
+    if not len(lines) == 2:
+        raise error(
+            """character groups must have exactly 2 lines:
+               the character and keyword on the first line separated by a space,
+               and the story or mnemonic on the second line"""
+        )
+
+    character_and_keyword = lines[0]
+    match = character_and_keyword_re.search(character_and_keyword)
+    if not match:
+        raise error(f"The following is not a character group:\n{paragraph}")
+    character, keyword = match.groups()
+    frame_number = character_to_frame_number(character)
+    story = "\n".join(lines[1:])
+    return CharacterGroup(
+        character=character, keyword=keyword, frame_number=frame_number, story=story
+    )
+
+
+def is_character_group(paragraph: str) -> bool:
+    return bool(ends_with_keyword_re.search(paragraph))
+
 
 def get_notes(text_file: Union[str, Path]) -> List[NoteGroup]:
     if not Path(text_file).is_file():
-        err_message = f"'{text_file}' needs to be a file containing notes"
-        logging.error(err_message)
-        raise ValueError(err_message)
+        raise error(f"'{text_file}' needs to be a file containing notes")
 
     text = Path(text_file).read_text()
-    groups = text.split("\n\n")
-    filter_comments = lambda par: "\n".join(line for line in par.split("\n") if not comment_or_blank(line))
+    groups = split_groups(text)
+    filter_comments = lambda par: "\n".join(
+        filter(not_comment_or_blank, par.split("\n"))
+    )
     filtered_groups = map(filter_comments, groups)
-    for group in filtered_groups:
-        pass
-
-    raise NotImplementedError(":(")
+    filter_characters = (
+        lambda par: parse_character_group(par) if is_character_group(par) else par
+    )
+    characters_filtered = map(filter_characters, filtered_groups)
+    filter_vocab = (
+        lambda par: parse_vocab_group(par) if is_character_group(par) else par
+    )
+    vocab_filtered = map(filter_vocab, characters_filtered)
+    return list(vocab_filtered)
 
 
 def generate_pictures(media_dir: SPath, picture_text: List[str]) -> Pictures:
